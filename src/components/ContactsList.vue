8804487f2b219dbed242307d221b7359552c28f1
pedja77 <pedja77@stcable.net>
2018-04-23 20:59:06 +0200

    first Vue component

---
+++ src/components/ContactsList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+   <ul>
+     <table>
+       <thead>
+         <th>First Name</th>
+         <th>Last Name</th>
+         <th>Email</th>
+       </thead>
+       <tbody>
+         <tr v-for="(user, key) in users" :key="key">
+          <td>{{user.firstName}}</td>
+           <td>{{user.lastName}}</td>
+           <td>{{user.email}}</td>
+         </tr>
+       </tbody>
+     </table>
+   </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactsList',
+  data() {
+    return {
+      users: [
+        {firstName: 'Pedja', lastName: 'Smigeljski', email: 'p@p.p'},
+        {firstName: 'Viki', lastName: 'Rakic', email: 'v@v.v'}, 
+        {firstName: 'Sofronije', lastName: 'Leghorn', email: 's@s.s'}
+        ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
