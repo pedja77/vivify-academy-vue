fc3dbda707c533da698f414767c9c55639c69c2f
pedja77 <pedja77@stcable.net>
2018-04-23 21:48:34 +0200

    removing users from table

--- src/components/ContactsList.vue
+++ src/components/ContactsList.vue
@@ -1,21 +1,30 @@
 <template>
   <div>
-   <ul>
+    <form @submit.prevent="addUser">
+      <label for="first-name">First name</label>
+      <input type="text" id="first-name" v-model="newUser.firstName">
+      <label for="last-name">Last name</label>
+      <input type="text" id="last-name" v-model="newUser.lastName">
+      <label for="email">Email</label>
+      <input type="text" id="email" v-model="newUser.email">
+      <button type="submit">Submit</button>
+    </form>
      <table>
        <thead>
          <th>First Name</th>
          <th>Last Name</th>
          <th>Email</th>
+         <th>&nbsp;</th>
        </thead>
        <tbody>
-         <tr v-for="(user, key) in users" :key="key" v-if="user.firstName === 'Sofronije'">
+         <tr v-for="(user, key) in users" :key="key">
            <td>{{user.firstName}}</td>
            <td>{{user.lastName}}</td>
            <td>{{user.email}}</td>
+           <td><button @click="deleteUser(key)">x</button></td>
          </tr>
        </tbody>
-     </table>
-   </ul>
+    </table>
   </div>
 </template>
 
@@ -28,9 +37,24 @@ export default {
         {firstName: 'Pedja', lastName: 'Smigeljski', email: 'p@p.p'},
         {firstName: 'Viki', lastName: 'Rakic', email: 'v@v.v'}, 
         {firstName: 'Sofronije', lastName: 'Leghorn', email: 's@s.s'}
-        ]
+        ],
+      newUser: {
+        firstName: '',
+        lastName: '',
+        email: ''
+      }
     }
-  }
+  },
+  methods: {
+    addUser() {
+      //console.log(event);
+      this.users.push(this.newUser);
+      this.newUser = {} // nova referenca na objekat newUser, ispraznimo inpute
+    },
+    deleteUser(index) {
+      this.users.splice(index, 1);
+    }
+ }
 }
 </script>
 
